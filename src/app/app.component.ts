565fd0961eadc813f0b24a4a99c843596a6d8266
rmarkowi1990 <rick@fastcasual.co>
2024-05-03 17:56:46 -0700

    now rendering dynamic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  template: `
+  <main>
+    <header class="brand-name">
+    <img class="brand-log" src="/assets/logo.svg" alt="logo" aria-hidden=true />
+    </header>
+  <section class="content">
+    <app-home></app-home>
+  </section>
+  <section class="results">
+   
+  </section>
+  </main>
+  `
+  ,
   styleUrls: ['./app.component.css'],
+  imports: [HomeComponent, HousingLocationComponent]
 })
 export class AppComponent {
   title = 'homes';
